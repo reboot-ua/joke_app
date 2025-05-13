496d6a88d69e71ccfeaa84b45280c35e8e400792
Sergiii Iakovyshyn <iakovyshyn.s@gmail.com>
2025-05-13 16:33:21 +0300

    add hook

--- src/store/jokes/api/jokesApi.ts
+++ src/store/jokes/api/jokesApi.ts
@@ -3,30 +3,22 @@ import { Joke } from '../jokesTypes';
 
 const API_URL = 'https://official-joke-api.appspot.com/jokes';
 
-/**
- * Функція для отримання одного випадкового жарту.
- * @returns Promise з жартом або null, якщо жарт не вдалося отримати
- */
 export const fetchRandomJoke = async (): Promise<Joke | null> => {
     try {
         const response = await axios.get<Joke>(`${API_URL}/random`);
         return response.data;
     } catch (error) {
         console.error('Error fetching random joke:', error);
-        return null; // Повертаємо null, якщо виникла помилка
+        return null;
     }
 };
 
-/**
- * Функція для отримання 10 жартів.
- * @returns Promise з масивом жартів
- */
 export const fetchTenJokes = async (): Promise<Joke[]> => {
     try {
         const response = await axios.get<Joke[]>(`${API_URL}/ten`);
         return response.data;
     } catch (error) {
         console.error('Error fetching 10 jokes:', error);
-        return []; // Повертаємо порожній масив, якщо виникла помилка
+        return [];
     }
 };
